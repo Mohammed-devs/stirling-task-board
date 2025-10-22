db84aaaf556e8f487b749a87f29215b16115b531
Mohammed-devs <daneal.sad@gmail.com>
2025-10-22 15:36:30 +0300

    Configure path alias in Vite config
    Added path aliasing for the 'src' folder.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // <-- Make sure to import 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // This forces the output to a simple 'dist' folder
-    outDir: 'dist',
-  }
+  resolve: {
+    alias: {
+      // This line tells Vite that '@' is a shortcut for the 'src' folder
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
