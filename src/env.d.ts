e7ce0b03cc8c9322662b08bcd33ab4d06d38fbe1
Mohammed-devs <daneal.sad@gmail.com>
2025-10-22 13:18:01 +0300

    Add DB property to Env interface
    Added DB property to Env interface.

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,6 +2,7 @@ export {};
 
 declare global {
   interface Env {
+        DB: any; // <-- ADD THIS LINE
     // If your worker uses environment variables or bindings (like KV, D1),
     // you would define them here. For now, an empty interface is enough
     // to fix the error.
